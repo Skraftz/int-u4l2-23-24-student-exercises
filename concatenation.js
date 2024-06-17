88f5f2d2c5ae48ecbd98c1e358969e3756956db3
Skraftz <davekenkent@gmail.com>
2024-06-17 13:56:59 -0400

    c

--- concatenation.js
+++ concatenation.js
@@ -7,13 +7,23 @@ submitButton.addEventListener("click", function() {
     let mood = document.querySelector(".mood").value;
     let food = document.querySelector(".food-name").value;
     let birthYear = document.querySelector(".year").value;
-    let age = 2023 - birthYear; // update to 2024, if necessary
+    let age = 2024 - birthYear; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
-
+    let city = document.querySelector(".city").value;
     // 2. Below write your sentences! 
     // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
-
+    if (age > 122) {
+        document.querySelector(".results").innerHTML ="Not a valid birth year.";
+    } else {
+    let arrest;
+    if (age >= 18){
+        arrest = "I might be legally liable, at " + age + ", but who's gonna stop me? 🤪. ";
+    } else {
+        arrest = "They can't legally arrest me, because I'm only " + age +", so we gonna go hard! 🔥. ";
+    };
+    document.querySelector(".results").innerHTML = "What's up my giggity ganstars? I'm DJ Lil' " + myName + ", and I'm feeling so " +mood+" rn. Today, we're gonna blow up the " +food+" factory, located in "+city+"!!!! " + arrest + " Let's get into this epic video!"
+};
 
 });
--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-let buttonOne = document.querySelector(".button-one");
+let ButtonOne = document.querySelector(".button-one");
 let captionOne = document.querySelector(".caption-one");
+let ButtonTwo = document.querySelector(".button-two");
+let captionTwo = document.querySelector(".caption-two");
 
-buttonOne.addEventListener("click", function() {
+
+ButtonOne.addEventListener("click", function() {
+    let spidyCaption = document.querySelector(".comment-one");
     // 1. Declare a variable named spidyCaption.
     //  - Store the value of the input with a class of "comment-one".
 
-
+    captionOne.innerHTML = spidyCaption.value;
     // 2. Display the variable spidyCaption to captionOne.
 
 
 });
 
+ButtonTwo.addEventListener("click", function() {
+    let spidyCaption = document.querySelector(".comment-two");
+    // 1. Declare a variable named spidyCaption.
+    //  - Store the value of the input with a class of "comment-Two".
+
+    captionTwo.innerHTML = spidyCaption.value;
+    // 2. Display the variable spidyCaption to captionTwo.
+    
+
+});
 // 3. Repeat the previous steps for the second meme.
-//  - Be sure to store variables for the button and caption.
+//  - Be sure to store variables for the Button and caption.
